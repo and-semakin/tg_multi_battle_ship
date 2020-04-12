3d05b038ce1991fa846be3ed8587009877ef0785
Andrey Semakin <and-semakin@ya.ru>
2020-04-12 17:13:18 +0500

    Add inline buttons

--- telegram.py
+++ telegram.py
@@ -1,10 +1,23 @@
+import logging
+from typing import Union
+
 import telebot
 from dotenv import load_dotenv
-from telebot.types import Message
+from more_itertools import chunked
+from telebot.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
 
 from config import Config
-from game import CellState, GameError, Player
+from game import LETTERS, CellState, GameError, Player
 from game_master import GameMaster, GameMasterError
+
+BUTTONS_IN_A_ROW = 5
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -29,24 +42,55 @@ STATE_ANSWERS = {
 }
 
 
+_position_buffer: str = ""
+
+
 def _get_game_id(message: Message) -> str:
     return str(message.chat.id)
 
 
-def _get_sender_player(message: Message) -> Player:
+def _get_sender_player(message: Union[Message, CallbackQuery]) -> Player:
     return Player(str(message.from_user.id), message.from_user.first_name)
+
+
+def _create_letters_inline_markup() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup()
+    buttons = []
+    for letter in LETTERS:
+        button_text = letter.upper()
+        buttons.append(InlineKeyboardButton(button_text, callback_data=button_text))
+    for buttons_row in chunked(buttons, BUTTONS_IN_A_ROW):
+        markup.row(*buttons_row)
+    return markup
+
+
+def _create_numbers_inline_markup() -> InlineKeyboardMarkup:
+    markup = InlineKeyboardMarkup()
+    buttons = []
+    for number in range(1, 11):
+        button_text = str(number)
+        buttons.append(InlineKeyboardButton(button_text, callback_data=button_text))
+    for buttons_row in chunked(buttons, BUTTONS_IN_A_ROW):
+        markup.row(*buttons_row)
+    return markup
 
 
 def _next_move(chat_id: int, game_id: str) -> None:
     game = master.games[game_id]
     attacker, victim, number = game.current_move
+
+    markup = _create_letters_inline_markup()
     if number > 1:
         bot.send_message(
-            chat_id, f"{attacker.name} ходит на {victim.name} {number} раз..."
+            chat_id,
+            f"{attacker.name} ходит на {victim.name} {number} раз...",
+            reply_markup=markup,
         )
 
     else:
-        bot.send_message(chat_id, f"{attacker.name} ходит на {victim.name}...")
+        bot.send_message(
+            chat_id, f"{attacker.name} ходит на {victim.name}...", reply_markup=markup,
+        )
 
 
 def _check_if_message_is_game(message: Message) -> bool:
@@ -86,6 +130,33 @@ def _check_if_message_is_game(message: Message) -> bool:
 
         if text not in STATE_ANSWERS:
             return False
+
+    return True
+
+
+def _check_if_callback_is_valid(call: CallbackQuery) -> bool:
+    game_id = _get_game_id(call.message)
+
+    if game_id not in master.games:
+        return False
+
+    game = master.games[game_id]
+    if not game.started:
+        return False
+
+    player = _get_sender_player(call)
+    if player not in game.players:
+        return False
+
+    attacker, victim, _ = game.current_move
+    text = call.data.strip().lower()
+
+    if game_id in master._moves:
+        return False
+
+    # attacker request
+    if player != attacker:
+        return False
 
     return True
 
@@ -178,6 +249,39 @@ def handle_message(message: Message) -> None:
         # victim response
         master.attack_response(game_id, player, STATE_ANSWERS[text])
         _next_move(message.chat.id, game_id)
+
+
+@bot.callback_query_handler(func=_check_if_callback_is_valid)
+def callback_handler(call: CallbackQuery) -> None:
+    global _position_buffer
+
+    logger.info(call)
+
+    game_id = _get_game_id(call.message)
+    player = _get_sender_player(call)
+    game = master.games[game_id]
+
+    attacker, victim, _ = game.current_move
+
+    if str(call.data).isnumeric():
+        # attack
+        _position_buffer += call.data
+        bot.edit_message_reply_markup(
+            call.message.chat.id, call.message.message_id, reply_markup=None
+        )
+        master.attack_request(game_id, player, victim, _position_buffer)
+        bot.reply_to(
+            call.message,
+            f"Принято, {_position_buffer.strip().upper()}. Что же там, {victim.name}?",
+        )
+        _position_buffer = ""
+    else:
+        # prepare attack
+        numbers_markup = _create_numbers_inline_markup()
+        _position_buffer += call.data
+        bot.edit_message_reply_markup(
+            call.message.chat.id, call.message.message_id, reply_markup=numbers_markup
+        )
 
 
 bot.polling()
